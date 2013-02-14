88e8e254033ad6e21fa17e9742166b8f4ad617c6
Steven Lindberg <stevenjohnlindberg@gmail.com>
2013-02-14 11:48:04 -0800

    Added CommonJS/AMD module export support

--- src/export.js
+++ src/export.js
@@ -1,1 +1,13 @@
-root['informant'] = informant;
+// Module export
+if (typeof module !== 'undefined') {
+  // CommonJS
+  module.exports = informant;
+} else if (typeof define === 'function' && define.amd) {
+  // AMD using a named module
+  define('informant', function() {
+    return informant;
+  });
+} else {
+  // Normal global
+  root['informant'] = informant;
+}
